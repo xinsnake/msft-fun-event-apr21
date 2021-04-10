50435428e23468cbceafeef2ff4c6d49291c00b4
Xinyun Zhou <me@xyzhou.com>
2021-04-11 00:33:26 +1000

    fix buttons to be real buttons

--- src/components/RouterPages/Section1SelectionPage.vue
+++ src/components/RouterPages/Section1SelectionPage.vue
@@ -1,50 +1,50 @@
 <template>
-  <div class="section-1-selection-page">
-    <div style="display: flex; margin-bottom: 20px;">
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #ffce91;">Software<br/>100</button>
-        <button style="background: #ffb152;">Software<br/>200</button>
-        <button style="background: #ff8c00">Software<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #ccd3a5;">Security<br/>100</button>
-        <button style="background: #c5d374;">Security<br/>200</button>
-        <button style="background: #bad80a;">Security<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #bad7df;">Database<br/>100</button>
-        <button style="background: #76cde6;">Database<br/>200</button>
-        <button style="background: #00bcf2;">Database<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #dfb3b7;">Architecture<br/>100</button>
-        <button style="background: #e27780;">Architecture<br/>200</button>
-        <button style="background: #e81123;">Architecture<br/>300</button>
-      </div>
-    </div>
-    <div style="display: flex; margin-top: 20px;">
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #646b8b;">Linux<br/>100</button>
-        <button style="background: #34428b;">Linux<br/>200</button>
-        <button style="background: #00188f;">Linux<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #a183a8;">Web<br/>100</button>
-        <button style="background: #955ea3;">Web<br/>200</button>
-        <button style="background: #68217a;">Web<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #d69fc0;">Office<br/>100</button>
-        <button style="background: #e075b6;">Office<br/>200</button>
-        <button style="background: #ec008c;">Office<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #9ccfb4;">Theory<br/>100</button>
-        <button style="background: #68c594;">Theory<br/>200</button>
-        <button style="background: #009e49;">Theory<br/>300</button>
-      </div>
-    </div>
-  </div>
+  <b-container class="section-1-selection-page">
+    <b-row>
+      <b-col>
+        <b-button block variant="primary" size="lg">Software 100</b-button>
+        <b-button block variant="primary" size="lg">Software 200</b-button>
+        <b-button block variant="primary" size="lg">Software 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="success" size="lg">Security 100</b-button>
+        <b-button block variant="success" size="lg">Security 200</b-button>
+        <b-button block variant="success" size="lg">Security 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="warning" size="lg">Database 100</b-button>
+        <b-button block variant="warning" size="lg">Database 200</b-button>
+        <b-button block variant="warning" size="lg">Database 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="outline-dark" size="lg">Architecture 100</b-button>
+        <b-button block variant="outline-dark" size="lg">Architecture 200</b-button>
+        <b-button block variant="outline-dark" size="lg">Architecture 300</b-button>
+      </b-col>
+    </b-row>
+    <b-row class="mt-2">
+      <b-col>
+        <b-button block variant="dark" size="lg">Linux 100</b-button>
+        <b-button block variant="dark" size="lg">Linux 200</b-button>
+        <b-button block variant="dark" size="lg">Linux 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="danger" size="lg">Web 100</b-button>
+        <b-button block variant="danger" size="lg">Web 200</b-button>
+        <b-button block variant="danger" size="lg">Web 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="info" size="lg">Office 100</b-button>
+        <b-button block variant="info" size="lg">Office 200</b-button>
+        <b-button block variant="info" size="lg">Office 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="secondary" size="lg">Theory 100</b-button>
+        <b-button block variant="secondary" size="lg">Theory 200</b-button>
+        <b-button block variant="secondary" size="lg">Theory 300</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
--- src/components/RouterPages/Section3SelectionPage.vue
+++ src/components/RouterPages/Section3SelectionPage.vue
@@ -1,50 +1,50 @@
 <template>
-  <div class="section-3-selection-page">
-    <div style="display: flex; margin-bottom: 20px;">
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #ffce91;">History<br/>100</button>
-        <button style="background: #ffb152;">History<br/>200</button>
-        <button style="background: #ff8c00">History<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #ccd3a5;">Biology<br/>100</button>
-        <button style="background: #c5d374;">Biology<br/>200</button>
-        <button style="background: #bad80a;">Biology<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #bad7df;">Physics<br/>100</button>
-        <button style="background: #76cde6;">Physics<br/>200</button>
-        <button style="background: #00bcf2;">Physics<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #dfb3b7;">Chemistry<br/>100</button>
-        <button style="background: #e27780;">Chemistry<br/>200</button>
-        <button style="background: #e81123;">Chemistry<br/>300</button>
-      </div>
-    </div>
-    <div style="display: flex; margin-top: 20px;">
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #646b8b;">Politics<br/>100</button>
-        <button style="background: #34428b;">Politics<br/>200</button>
-        <button style="background: #00188f;">Politics<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #a183a8;">Geology<br/>100</button>
-        <button style="background: #955ea3;">Geology<br/>200</button>
-        <button style="background: #68217a;">Geology<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #d69fc0;">Economy<br/>100</button>
-        <button style="background: #e075b6;">Economy<br/>200</button>
-        <button style="background: #ec008c;">Economy<br/>300</button>
-      </div>
-      <div style="flex: 1; display: flex; flex-direction: column; margin: 0 2px; font-size: 35px;">
-        <button style="background: #9ccfb4;">Business<br/>100</button>
-        <button style="background: #68c594;">Business<br/>200</button>
-        <button style="background: #009e49;">Business<br/>300</button>
-      </div>
-    </div>
-  </div>
+  <b-container class="section-3-selection-page">
+    <b-row>
+      <b-col>
+        <b-button block variant="primary" size="lg">History 100</b-button>
+        <b-button block variant="primary" size="lg">History 200</b-button>
+        <b-button block variant="primary" size="lg">History 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="success" size="lg">Biology 100</b-button>
+        <b-button block variant="success" size="lg">Biology 200</b-button>
+        <b-button block variant="success" size="lg">Biology 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="warning" size="lg">Physics 100</b-button>
+        <b-button block variant="warning" size="lg">Physics 200</b-button>
+        <b-button block variant="warning" size="lg">Physics 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="outline-dark" size="lg">Chemistry 100</b-button>
+        <b-button block variant="outline-dark" size="lg">Chemistry 200</b-button>
+        <b-button block variant="outline-dark" size="lg">Chemistry 300</b-button>
+      </b-col>
+    </b-row>
+    <b-row class="mt-2">
+      <b-col>
+        <b-button block variant="dark" size="lg">Politics 100</b-button>
+        <b-button block variant="dark" size="lg">Politics 200</b-button>
+        <b-button block variant="dark" size="lg">Politics 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="danger" size="lg">Geology 100</b-button>
+        <b-button block variant="danger" size="lg">Geology 200</b-button>
+        <b-button block variant="danger" size="lg">Geology 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="info" size="lg">Economy 100</b-button>
+        <b-button block variant="info" size="lg">Economy 200</b-button>
+        <b-button block variant="info" size="lg">Economy 300</b-button>
+      </b-col>
+      <b-col>
+        <b-button block variant="secondary" size="lg">Business 100</b-button>
+        <b-button block variant="secondary" size="lg">Business 200</b-button>
+        <b-button block variant="secondary" size="lg">Business 300</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
