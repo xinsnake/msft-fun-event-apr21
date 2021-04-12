c0814348c8ba375347ba64fb0eeb7ff46a70fe21
Xinyun Zhou <me@xyzhou.com>
2021-04-13 01:20:30 +1000

    adding question structure

--- src/store/modules/multipleChoices.js
+++ src/store/modules/multipleChoices.js
@@ -1,4 +1,43 @@
 const state = () => ({
+  s1: {
+    software100: {
+      points: 100,
+      question: 'This is a sample question',
+      answer: {
+        a: 'answer 1',
+        b: 'answer 2',
+        c: 'answer 3',
+        d: 'answer 4'
+      },
+      selected: false
+    },
+    software200: {},
+    software300: {},
+    security100: {},
+    security200: {},
+    security300: {},
+    database100: {},
+    database200: {},
+    database300: {},
+    architecture100: {},
+    architecture200: {},
+    architecture300: {},
+    linux100: {},
+    linux200: {},
+    linux300: {},
+    web100: {},
+    web200: {},
+    web300: {},
+    office100: {},
+    office200: {},
+    office300: {},
+    theory100: {},
+    theory200: {},
+    theory300: {}
+  },
+  s3: {
+
+  }
 })
 
 const getters = {}
