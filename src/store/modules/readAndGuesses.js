1cc5642f0972fdae38687f3d6f65c234c52e10b8
Jacob Zhou <me@xyzhou.com>
2021-05-06 23:22:54 +1000

    Update readAndGuesses.js

--- src/store/modules/readAndGuesses.js
+++ src/store/modules/readAndGuesses.js
@@ -1,21 +1,21 @@
 const state = () => ({
   azure1: {
-    1: 'This is sentence 1',
-    2: 'This is sentence 2',
-    3: 'This is sentence 3',
-    4: 'This is sentence 4',
-    5: 'This is sentence 5',
-    6: 'This is sentence 6',
-    answer: 'This is the Answer'
+    1: 'It guaranteed speed at any scale – even through bursts – with instant, limitless elasticity.',
+    2: 'It has fast, flexible app development with SDKs for popular languages and no-ETL (extract, transform, load) analytics.',
+    3: 'It is ready for mission-critical applications with guaranteed business continuity, 99.999-per cent availability and enterprise-level security.',
+    4: 'It ensure business continuity with turnkey multi-master replication and enterprise-grade security and compliance, including end-to-end encryption and access control.',
+    5: 'It is a fully managed and cost-effective serverless database.',
+    6: 'It has a native Core (SQL) API along with APIs for MongoDB, Cassandra and Gremlin.',
+    answer: 'Azure Cosmos DB'
   },
   azure2: {
-    1: 'This is sentence 1',
-    2: 'This is sentence 2',
-    3: 'This is sentence 3',
-    4: 'This is sentence 4',
-    5: 'This is sentence 5',
-    6: 'This is sentence 6',
-    answer: 'This is the Answer'
+    1: 'It eliminates polling – and the associated cost and latency.',
+    2: 'You can build better, more reliable applications through reactive programming, capitalising on guaranteed event delivery and the high availability of the cloud.',
+    3: 'It is designed for high availability, consistent performance and dynamic scale, and lets you focus on your app logic rather than infrastructure.',
+    4: 'It helps you to develop richer application scenarios by connecting multiple possible sources and destinations of events.',
+    5: 'With this service, event publishers are decoupled from event subscribers using a pub/sub model and simple HTTP-based event delivery',
+    6: 'It allows your business logic can be triggered by virtually all Azure services, as well as custom sources.',
+    answer: 'Event Grid'
   },
   azure3: {
     1: 'This is sentence 1',
